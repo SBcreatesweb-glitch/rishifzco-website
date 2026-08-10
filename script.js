1218765983350b1b4c30654d47473da330ae94ee
SBcreatesweb-glitch <v88p5thdd7@privaterelay.appleid.com>
2026-08-11 03:12:59 +0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Script Loaded");
+document.addEventListener("DOMContentLoaded", function () {
+
+    const navLinks = document.querySelectorAll(".navbar-nav .nav-link");
+    const navbarCollapse = document.querySelector(".navbar-collapse");
+
+    navLinks.forEach(function (link) {
+
+        link.addEventListener("click", function () {
+
+            if (navbarCollapse.classList.contains("show")) {
+
+                const bsCollapse = bootstrap.Collapse.getInstance(navbarCollapse);
+
+                if (bsCollapse) {
+                    bsCollapse.hide();
+                }
+
+            }
+
+        });
+
+    });
+
+});
